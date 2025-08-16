7cf6a4a5a3567bb46f7c6a290fd54a3407dae0b7
Gaus Saraf Murady <63799660+b1tranger@users.noreply.github.com>
2025-08-16 22:26:59 +0600

    updated code

---
+++ C++/Codes/STL/stack.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+cout << "\n\n################\n\t[ 1 ]\t\n################\n\n";
+
+
+
+
+
+    return 0;
+}
--- C++/Codes/STL/vector.cpp
+++ C++/Codes/STL/vector.cpp
@@ -3,17 +3,39 @@
 using namespace std;
 
 int main(){
+
+cout << "\n\n################\n\t[ 1 ]\t\n################\n\n";
+
 vector<int> v1 = {1,2,3,4,5};
 
 v1.push_back(6);
 //v1.push_back(7);
 v1.pop_back();
 
+cout<<"\nChecking manual insertion, push_back(), pop_back()\n";
+
 for(int i = 0; i < v1.size();i++){
     cout << v1[i] << " "; //1 2 3 4 5 
 }
 
-auto it = find(v1.begin(),v1.end(),3);
+// auto it = find(v1.begin(),v1.end(),3);
+
+cout << "\n\n################\n\t[ 2 ]\t\n################\n\n";
+
+vector<int> v2;
+
+
+cout<<"\nChecking size() of v1\n";
+cout << v1.size();
+cout<<"\nChecking front(), back() of v1\n";
+cout << v1.front() << " " << v1.back();
+cout<<"\nChecking empty() of v1\n";
+cout << v1.empty();
+cout<<"\nUsing clear(), Checking empty() of v1\n";
+v1.clear();
+cout << v1.empty();
+
+
 
 
     return 0;
