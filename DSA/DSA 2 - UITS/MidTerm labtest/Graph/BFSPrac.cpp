c769063c70127544b1f80519f7136d1c66ea8cbb
BitRanger <63799660+b1tranger@users.noreply.github.com>
2025-10-18 22:15:01 +0600

    Create BFSPrac.cpp

---
+++ DSA/DSA 2 - UITS/MidTerm labtest/Graph/BFSPrac.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // node, edge
+    int node, edge;
+    cin >> node >> edge;
+    // loop until edge
+    for(int i=0;i<edge;i++){
+
+
+    }
+
+    return 0;
+}
