3e7903d1e7085177b00de64082bd2922dfee91fb
BitRanger <63799660+b1tranger@users.noreply.github.com>
2026-01-10 03:23:55 +0600

    Update leetcode_stwo-sum.cpp

--- CLI-online-judge/C++/LeetCode/leetcode_stwo-sum.cpp
+++ CLI-online-judge/C++/LeetCode/leetcode_stwo-sum.cpp
@@ -4,40 +4,35 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int 
-        for(int i=0;i<nums.size()-1;i++){
-             for(int j=i+1;j<nums.size();j++){
-if(num)
-             }
-        }
+        //    int sum = 0;
+        vector<int> ans;
 
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                   ans = {i,j}; 
+                }
+            }
+        }
+        return ans;
     }
 };
 
 int main()
 {
-    vector<int> nums;
+    vector<int> nums,ans;
     int target;
     int n;
     cin >> n;
     int x;
-    for(int i = 0; i <n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> x;
         nums.push_back(x);
     }
+    cin >> target;
     Solution obj;
-    obj.twoSum(nums,target);
-    for(int i = 0; i <n; i++)
-    {
-//        cin >> x;
-//        nums.push_back(x);
-        cout << nums[i] << " " ;
-    }
-
-
-
-
-
+     ans = obj.twoSum(nums, target);
+     printf("[%d,%d]",ans[0],ans[1]);
     return 0;
 }
