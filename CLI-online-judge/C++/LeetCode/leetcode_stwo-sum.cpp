ce05a711b3bd3188b0262c067b7897e13a135f76
BitRanger <63799660+b1tranger@users.noreply.github.com>
2026-01-09 23:46:07 +0600

    Update leetcode_stwo-sum.cpp

--- CLI-online-judge/C++/LeetCode/leetcode_stwo-sum.cpp
+++ CLI-online-judge/C++/LeetCode/leetcode_stwo-sum.cpp
@@ -1,22 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int 
+        for(int i=0;i<nums.size()-1;i++){
+             for(int j=i+1;j<nums.size();j++){
+if(num)
+             }
+        }
+
+    }
+};
+
 int main()
 {
-    vector<int> arr;
+    vector<int> nums;
+    int target;
     int n;
     cin >> n;
     int x;
     for(int i = 0; i <n; i++)
     {
         cin >> x;
-        arr.push_back(x);
+        nums.push_back(x);
     }
+    Solution obj;
+    obj.twoSum(nums,target);
     for(int i = 0; i <n; i++)
     {
 //        cin >> x;
-//        arr.push_back(x);
-        cout << arr[i] << " " ;
+//        nums.push_back(x);
+        cout << nums[i] << " " ;
     }
 
 
